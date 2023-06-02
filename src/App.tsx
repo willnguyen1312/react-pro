527e48105827f23353d2a972ffbb1b508f53830b
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-02 08:40:07 -0300

    🎢

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+import { CircularProgress } from "./components/CircleProgress";
 
 function App() {
-  const [progress, setProgress] = useState(1);
-  const circularProgressRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const circularProgress = circularProgressRef.current as HTMLDivElement;
-
-    let progressStartValue = 0;
-    const progressEndValue = 90;
-    const speed = 100;
-
-    const progress = setInterval(() => {
-      // progressStartValue++;
-      // progressValue.textContent = `${progressStartValue}%`;
-      setProgress(progressStartValue++);
-      circularProgress.style.background = `conic-gradient(#306285 ${
-        progressStartValue * 3.6
-      }deg, #ededed 0deg)`;
-      if (progressStartValue === progressEndValue) {
-        clearInterval(progress);
-      }
-    }, speed);
-  }, []);
-
   return (
     <div className="p-4">
-      <div ref={circularProgressRef} className="circular-progress">
-        <span className="relative font-600 text-[40px] text-[#306285]">
-          {`${progress}%`}
-        </span>
-      </div>
+      <CircularProgress />
     </div>
   );
 }
