82c4ef51d521910fa0de09cac53990ad86da673b
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-11-15 14:41:35 -0400

    oh boy 😁

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react";
+
 function App() {
+    useEffect(() => {
+        try {
+            setTimeout(() => {
+                try {
+                    // throw new Error("Something went wrong");
+                } catch (error) {
+                    console.log(error + "nha");
+                }
+            }, 3000);
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
     return (
         <form>
-            <input
-                aria-invalid
-                aria-describedby="error-message"
-                type="text"
-                className="w-32 p-2 border rounded"
-            />
-
-            <div id="error-message">Something is wrong</div>
+            <div id="error-message">Okela</div>
+            <button
+                type="button"
+                onClick={() => {
+                    throw new Error("Something went wrong from button");
+                }}
+            >
+                Click me
+            </button>
         </form>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import "@unocss/reset/tailwind.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import "virtual:uno.css";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <App />
+        </ErrorBoundary>
     </React.StrictMode>,
 );
