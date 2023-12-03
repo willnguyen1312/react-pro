0ce004e2ea3866fd1422a610f354de36c45b4713
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-03 18:57:04 -0400

    Rock and roll with zoom image on hover 🐻‍❄️

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,71 @@
-import { useEffect } from "react";
+import { useZoomImageHover } from "@zoom-image/react";
+import { useEffect, useRef, useState } from "react";
+
+const getMeta = (url: string): Promise<HTMLImageElement> =>
+    new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(err);
+        img.src = url;
+    });
 
 function App() {
+    const imageHoverContainerRef = useRef<HTMLDivElement>(null);
+    const zoomTargetRef = useRef<HTMLDivElement>(null);
+    const { createZoomImage: createZoomImageHover } = useZoomImageHover();
+    const [aspectRatio, setAspectRatio] = useState(0);
+
     useEffect(() => {
-        try {
-            setTimeout(() => {
-                try {
-                    // throw new Error("Something went wrong");
-                } catch (error) {
-                    console.log(error + "nha");
-                }
-            }, 3000);
-        } catch (error) {
-            console.log(error);
+        async function createZoomImage() {
+            const image = await getMeta(
+                "https://willnguyen1312.github.io/zoom-image/sample.avif",
+            );
+            const { naturalHeight, naturalWidth } = image;
+            const aspectRatio = naturalWidth / naturalHeight;
+            const CONTAINER_WIDTH = 200;
+            const CONTAINER_HEIGHT = CONTAINER_WIDTH / aspectRatio;
+
+            const imageContainer =
+                imageHoverContainerRef.current as HTMLDivElement;
+
+            imageContainer.style.width = `${CONTAINER_WIDTH}px`;
+            imageContainer.style.height = `${CONTAINER_HEIGHT}px`;
+
+            const zoomTarget = zoomTargetRef.current as HTMLDivElement;
+            createZoomImageHover(imageContainer, {
+                zoomImageSource:
+                    "https://willnguyen1312.github.io/zoom-image/sample.avif",
+                customZoom: { width: 300, height: 500 },
+                zoomTarget,
+                scale: 4,
+            });
+
+            setAspectRatio(aspectRatio);
         }
+
+        createZoomImage();
     }, []);
+
     return (
-        <form>
-            <div id="error-message">Okela</div>
-            <button
-                type="button"
-                onClick={() => {
-                    throw new Error("Something went wrong from button");
-                }}
+        <div className="p-4 font-sans">
+            <p>Hover inside the image to see zoom effect</p>
+            <div
+                ref={imageHoverContainerRef}
+                className={`relative ${
+                    aspectRatio ? "flex" : "hidden"
+                } items-start`}
             >
-                Click me
-            </button>
-        </form>
+                <img
+                    className="h-full w-full"
+                    alt="Small Pic"
+                    src="https://willnguyen1312.github.io/zoom-image/sample.avif"
+                />
+                <div
+                    ref={zoomTargetRef}
+                    className="absolute left-[350px]"
+                ></div>
+            </div>
+        </div>
     );
 }
 
