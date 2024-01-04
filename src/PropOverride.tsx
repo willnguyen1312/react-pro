42f3b1cc8ab433cfa450a1e7a0904d9a70dec481
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-04 08:40:08 -0400

    Add PropOverride stuff 🚀

---
+++ src/PropOverride.tsx
@@ -0,0 +1,21 @@
+import * as V from "varsace";
+import Logo from "./Logo";
+import { css } from "./css-hooks";
+
+const Child = ({ content = "Hello from child" }: { content?: string }) => {
+    return <h1>{content}</h1>;
+};
+
+const Parent = ({ content }: { content?: string }) => {
+    return <Child content={content} />;
+};
+
+function App() {
+    const props = {
+        content: "Hello from props",
+    };
+
+    return <Parent {...props} content="wakanda" />;
+}
+
+export default App;
