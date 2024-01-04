04310b45a559028d502029717844f0eecaa30796
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-04 10:19:52 -0400

    Oh ke

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,14 @@
 import React from "react";
 
-function Child({ show }: { show: boolean }) {
-    React.useEffect(() => {
-        console.log("Child mounted");
-
-        return () => {
-            console.log("Child unmounted");
-        };
-    }, []);
-
-    console.log("Child rendered");
-
-    return show ? <h1>I am a child</h1> : null;
-}
-
-function Parent() {
-    const [show, setShow] = React.useState(true);
-
-    return (
-        <div>
-            <h1 className="text-red">I am a parent</h1>
-            <button
-                onClick={() => {
-                    setShow(!show);
-                }}
-            >
-                Toggle show
-            </button>
-
-            {show && <Child show={show} />}
-        </div>
-    );
-}
-
-type Com = "Parent" | "Child";
-
-const Record = {
-    Parent,
-    Child,
-} as const;
-
-type Props =
-    | {
-          type: "Parent";
-      }
-    | { type: "Child"; show: boolean };
-
-const Comp = (props: Props) => {
-    const { type, ...rest } = props;
-    const Comp = Record[type] as any;
-
-    return <Comp {...rest} />;
-};
-
-export default function App() {
-    const [count, setCount] = React.useState(0);
+export function App() {
+    const data = [1];
 
     return (
         <>
-            <h1>Count: {count}</h1>
-            <button
-                onClick={() => {
-                    setCount(1);
-
-                    setCount(3);
-
-                    setCount(5);
-
-                    setCount((count) => count + 1);
-                }}
-            >
-                Increment
-            </button>
+            <h1>Data</h1>
+            {data.map((item) => (
+                <p key={item}>{item}</p>
+            ))}
         </>
     );
 }
