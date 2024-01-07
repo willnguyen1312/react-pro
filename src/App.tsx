4ff193a535a83bb0d2f03910b7cea9e05a20a002
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-06 22:13:47 -0400

    Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { AppProvider, Button } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { useEffect } from "react";
 
 export function App() {
-    const data = [1];
-
+    useEffect(() => {
+        document.addEventListener("mousedown", (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("Document clicked");
+        });
+    }, []);
     return (
-        <>
-            <h1>Data</h1>
-            {data.map((item) => (
-                <p key={item}>{item}</p>
-            ))}
-        </>
+        <AppProvider i18n={enTranslations}>
+            <Button
+                onClick={() => {
+                    console.log("Button clicked");
+                }}
+                children="Button"
+            />
+        </AppProvider>
     );
 }
