bfbe0024182c54c5938ce7d3d90469ea8617256d
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-07 18:54:14 -0400

    Upgrade deps 💞

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
+import { inspectReact } from "@namnode/vite-plugin-inspect-react";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), UnoCSS()],
+    plugins: [inspectReact(), react(), UnoCSS()],
     server: {
         port: 1312,
         host: true,
