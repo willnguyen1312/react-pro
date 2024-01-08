74de5ba1fc771f895f887cbaaf981d829e31a647
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-01-08 10:39:59 -0400

    💞

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import "virtual:uno.css";
 import App from "./App";
 
+import { injectedComponentName } from "@namnode/vite-plugin-inspect-react/utils";
+
+console.log(injectedComponentName);
+
 const rootElement = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
