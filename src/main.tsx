cebaa9d9936c939ced1b9c4dadc107468a66295a
Nam Nguyen <nam.nguyen@shopify.com>
2024-01-18 18:22:28 -0400

    Hello event system 👋

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
 import ReactDOM from "react-dom/client";
 import "virtual:uno.css";
-import App from "./App";
-
-import { injectedComponentName } from "@namnode/vite-plugin-inspect-react/utils";
-
-console.log(injectedComponentName);
+// import App from "./App";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
+
+const App = () => {
+  return (
+    <div
+      onClick={(event) => {
+        console.log(event.target);
+        console.log(event.currentTarget);
+      }}
+    >
+      <h1>Hi</h1>
+      <button>Click me</button>
+    </div>
+  );
+};
 
 root.render(
   <>
