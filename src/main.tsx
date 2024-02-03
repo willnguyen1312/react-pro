4c55389438264c2eeee1e5b393efc3a1d4e02182
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-02-03 11:09:49 -0400

    🙈

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,43 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "virtual:uno.css";
-// import App from "./App";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 const root = ReactDOM.createRoot(rootElement);
 
+function syncFunc() {
+  throw new Error("This is a sync error");
+}
+
+async function asyncFunc() {
+  throw new Error("This is an async error");
+}
+
 export const App = () => {
+  // syncFunc();
+
+  asyncFunc().catch((err) => {
+    console.error("Oh no: ", err);
+  });
+
+  // syncFunc();
+
+  // useEffect(() => {
+  //   syncFunc();
+  // }, []);
+
   return (
-    <div
-      onFocus={(event) => {
-        console.log(event.target);
-      }}
-    >
-      <p>This child is placed in the parent div.</p>
-      <input type="text" placeholder="Hi there" />
-      {/* {createPortal(
-        <p>This child is placed in the document body.</p>,
-        document.body,
-      )} */}
-    </div>
+    <>
+      <p>Simple app</p>
+      <button
+        onClick={() => {
+          syncFunc();
+        }}
+      >
+        Click me
+      </button>
+    </>
   );
 };
 
