a6cacd78ecac0d1ffb3a6f6e6508ee3de8bb0175
Nam Nguyen <nam.nguyen@shopify.com>
2024-05-03 09:42:31 -0300

    Hook FTW 🚀

--- src/Scroll.tsx
+++ src/Scroll.tsx
@@ -1,66 +1,42 @@
-import React from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
-export default class ScrollingList extends React.Component {
-  state = {
-    list: [1, 2, 3, 4, 5, 6, 7],
-  };
+export default function ScrollingList() {
+  const [list, setList] = useState([1, 2, 3, 4, 5, 6, 7]);
+  const listRef = useRef<HTMLDivElement>(null);
+  const prevListLength = useRef(list.length);
 
-  constructor(props: any) {
-    super(props);
-    this.listRef = React.createRef();
-  }
-
-  getSnapshotBeforeUpdate(_, prevState: any) {
-    // Are we adding new items to the list?
-    // Capture the scroll position so we can adjust scroll later.
-    if (prevState.list.length < this.state.list.length) {
-      const list = this.listRef.current;
-      return list.scrollTop;
+  useLayoutEffect(() => {
+    const listElement = listRef.current;
+    if (prevListLength.current < list.length && listElement) {
+      const scrollTop = listElement.scrollTop;
+      listElement.scrollTop = scrollTop;
     }
-  }
+    prevListLength.current = list.length;
+  }, [list]);
 
-  componentDidUpdate(_, __, snapshot) {
-    // If we have a snapshot value, we've just added new items.
-    // Adjust scroll so these new items don't push the old ones out of view.
-    // (snapshot here is the value returned from getSnapshotBeforeUpdate)
-    if (snapshot !== null) {
-      const list = this.listRef.current;
-      list.scrollTop = snapshot;
-    }
-  }
-
-  render() {
-    return (
-      <div
-        ref={this.listRef}
-        style={{
-          height: 200,
-          width: 200,
-          display: "flex",
-          flexDirection: "column",
-          gap: 10,
-          overflow: "scroll",
-        }}
-      >
-        {this.state.list.map((_, index) => {
-          return (
-            <button
-              key={index}
-              onClick={() => {
-                const newList = [...this.state.list].concat(
-                  this.state.list.length + 1,
-                );
-
-                this.setState({
-                  list: newList,
-                });
-              }}
-            >
-              {index + 1}
-            </button>
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div
+      ref={listRef}
+      style={{
+        height: 200,
+        width: 200,
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        overflow: "scroll",
+      }}
+    >
+      {list.map((item, index) => (
+        <button
+          key={index}
+          onClick={() => {
+            const newList = [...list, list.length + 1];
+            setList(newList);
+          }}
+        >
+          {item}
+        </button>
+      ))}
+    </div>
+  );
 }
