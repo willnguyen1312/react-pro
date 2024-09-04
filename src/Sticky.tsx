5f624d8c529ec5eab2d07293b567779fad5d8a70
Nam Nguyen <nam.nguyen@shopify.com>
2024-09-03 20:55:20 -0600

    Oh sticky 🥢

---
+++ src/Sticky.tsx
@@ -0,0 +1,88 @@
+import "./Sticky.css";
+
+export default function Sticky() {
+  return (
+    // <div className="wrapper">
+    //   <div className="example">
+    //     <div className="content one"></div>
+    //     <div className="content">
+    //       <div className="box fixed">fixed</div>
+    //     </div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //   </div>
+    //   <div className="example">
+    //     <div className="content one"></div>
+    //     <div className="content">
+    //       <div className="box sticky">sticky</div>
+    //     </div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //     <div className="content"></div>
+    //   </div>
+    // </div>
+    <div className="wrapper">
+      <h1>
+        Scroll to the bottom <small>it's worth it</small>
+      </h1>
+
+      <div className="box">
+        <div className="box-content">
+          <h2 className="title">Lorem ipsum dolor sit amet.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            porro eius corrupti sint necessitatibus quisquam unde voluptatum
+            tempora praesentium. Similique eos veritatis perspiciatis ipsa quis
+            impedit beatae animi fugit, earum vero unde ad fugiat, rem
+            laboriosam laudantium? Adipisci, vitae esse.
+          </p>
+          <p>
+            Iusto quia mollitia repellat, expedita eveniet molestias rerum
+            dolore adipisci nemo reiciendis reprehenderit est dolorem vero
+            aliquam, necessitatibus natus laborum veritatis esse! Expedita
+            voluptates, modi tempora soluta totam aspernatur, deserunt
+            laboriosam, nam excepturi repellendus esse sint id consequatur
+            aliquid a?
+          </p>
+          <p>
+            Cumque nemo, ipsam neque repellat cupiditate inventore quibusdam
+            accusantium suscipit perferendis voluptates porro nesciunt
+            distinctio veniam voluptate quas? Aliquam inventore ullam quae
+            officiis. Dolores, quas. Doloribus saepe eaque impedit. Ut rem,
+            error eos nihil voluptatibus ad sed placeat tenetur aperiam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            doloribus ex eum, ut, assumenda corrupti aut minus ab at impedit
+            saepe natus quo libero maxime, tempore deserunt iusto totam modi
+            minima esse aspernatur. Assumenda maiores voluptate culpa dolore aut
+            quis tempore debitis atque ullam eligendi! Inventore, ipsam porro!
+            Ipsum, ad!
+          </p>
+          <p>
+            Esse placeat explicabo itaque cumque molestiae ullam, voluptatum
+            ducimus quibusdam maxime aperiam ipsam fuga commodi earum facilis
+            minima nemo vero a necessitatibus enim amet quia nisi. Incidunt esse
+            veritatis, tempore at omnis quam adipisci provident voluptate
+            necessitatibus labore. Quis perferendis tempore earum, a enim
+            debitis voluptates at laboriosam? Nulla, expedita.
+          </p>
+          <p>
+            Odit reprehenderit repudiandae dignissimos soluta iste, quasi
+            quisquam incidunt nemo, exercitationem labore aut quis aliquam!
+            Deserunt accusamus omnis sint suscipit sit perferendis nesciunt
+            quas, excepturi fuga reiciendis similique dignissimos, iusto saepe
+            ratione eum odit et nobis, cumque culpa tenetur placeat. Nisi
+            placeat non, pariatur ullam quos aut temporibus debitis eos!
+          </p>
+        </div>
+        <p className="hello-there">Hello there</p>
+      </div>
+    </div>
+  );
+}
