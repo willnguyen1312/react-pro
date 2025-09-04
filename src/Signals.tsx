0947e84dc99556d00de98a11747815c9ced05d08
Nam Nguyen <nam.nguyen@shopify.com>
2025-09-04 16:37:24 -0600

    🚀

--- src/Signals.tsx
+++ src/Signals.tsx
@@ -1,45 +1,19 @@
-import {
-  batch,
-  Signal,
-  useSignal,
-  useSignalEffect,
-} from "@preact/signals-react";
-// import { useSignals } from "@preact/signals-react/runtime";
+import { signal } from "@preact/signals-react";
 
-// const countSignal = signal(0);
+const countSignal = signal(0);
 
 export default function App() {
-  // useSignals();
-
-  const countSignal = useSignal(0);
-
-  useSignalEffect(() => {
-    console.log("countSignal changed", countSignal.value);
-  });
-
   return (
     <div>
-      <h1>Count value: </h1>
-      <ValueDisplay countSignal={countSignal} />
+      <h1>Count value: {countSignal.value}</h1>
 
       <button
         onClick={() => {
-          // batch(() => {
           countSignal.value += 1;
-          countSignal.value += 1;
-          countSignal.value += 1;
-          countSignal.value += 1;
-          countSignal.value += 1;
-          // });
         }}
       >
         Increment
       </button>
     </div>
   );
-}
-
-function ValueDisplay(props: { countSignal: Signal<number> }) {
-  console.log("ValueDisplay render");
-  return <h1>Value: {props.countSignal.value}</h1>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
