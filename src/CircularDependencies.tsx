a03a87eb66cab8c7b35353084c41e5f79e39c5a0
Nam Nguyen <nam.nguyen@shopify.com>
2025-09-10 10:07:25 -0600

    🏖️

--- src/CircularDependencies.tsx
+++ src/CircularDependencies.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div>
       <div>Hello from CircularDependencies</div>
+      <h1>{greeting()}</h1>
       <button onClick={() => alert(greeting())}>Greet</button>
     </div>
   );
---
+++ src/b.ts
@@ -0,0 +1,3 @@
+import { a } from "./a";
+console.log("b.js: a =", a);
+export const b = "Hello from B";
